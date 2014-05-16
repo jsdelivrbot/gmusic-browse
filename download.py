50305bf689618e67a3484fcd4c726677f7e5f403
sangelone <angelone@gmail.com>
2014-05-16 03:27:31 -0700

    Adding a default sort to JSON

--- download.py
+++ download.py
@@ -3,10 +3,12 @@
 from os import getenv
 from json import dumps
 from getpass import getpass
+from operator import itemgetter
 from gmusicapi import Mobileclient
 
 conf_filename = (getenv('HOME') or getenv('USERPROFILE')) + '/.gmusic'
 retain_keys = ['rating', 'year', 'album', 'title', 'genre', 'playCount', 'artist']
+sort_key = 'year'
 
 
 def connect_api():
@@ -34,10 +36,15 @@ def connect_api():
 def filter_keys(l, keys):
     return [{k: item[k] for k in keys} for item in l]
 
+def sort_list_by_key(l, key):
+    return sorted(l, key=itemgetter(key), reverse=True) 
+
+
 if __name__ == '__main__':
     api = connect_api()
 
     if api.is_authenticated():
         library = filter_keys(api.get_all_songs(), retain_keys)
+        library = sort_list_by_key(library, sort_key)
         print dumps(library, sort_keys=True, indent=4, separators=(',', ': '))
         api.logout()
